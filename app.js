5135c896af491687eddc30ded3726890fc348b02
Davis Uhlig <duhlig@daviss-mbp.lan>
2017-06-26 08:57:25 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,148 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const expressValidator = require("express-validator");
+const mustacheExpress = require("mustache-express");
+const path = require("path");
+const session = require("express-session");
+const writeJson = require("write-json");
+const fs = require("file-system");
+const words = fs.readFileSync("/usr/share/dict/words", "utf-8").toLowerCase().split("\n");
+let word = words[Math.floor(Math.random()*words.length)];
+console.log(word);
+let letters = word.split("");
+let underscores = [];
+
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.engine("mustache", mustacheExpress());
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "mustache");
+
+app.use(bodyParser.json());
+app.use(expressValidator());
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.use(session({
+  secret: 'aswd',
+  resave: false,
+  saveUninitialized: false
+}));
+
+let messages = [];
+let correctLetters = [];
+let lettersGuessed = [];
+let guessesLeft = 8;
+
+letters.forEach(function(newUnderscore){
+  underscores.push("_");
+});
+
+app.get("/", function(req, res){
+  res.render("index", {underscore: underscores, correct: correctLetters, allLetters: lettersGuessed, guesses: guessesLeft, errors: messages});
+
+});
+app.get("/gameover", function(req, res){
+
+  res.render("gameover", {words: word});
+});
+
+app.get("/winner", function(req, res){
+  res.render("winner");
+});
+
+app.post("/", function(req, res){
+  let match = false;
+  messages = [];
+  req.checkBody("userGuess", "Please Guess A Letter").notEmpty();
+  req.checkBody("userGuess", "Only One Letter At A Time!!").isLength({max: 1});
+
+  let errors = req.validationErrors();
+
+  if (errors) {
+    errors.forEach(function(error) {
+      messages.push(error.msg);
+      match = true;
+    });
+  }
+
+  for (var i = 0; i < letters.length; i++) {
+    letter = letters[i]
+
+    if (letter === req.body.userGuess) {
+      correctLetters.push(letter);
+      underscores[i] = letters[i];
+      match = true;
+
+    }
+
+
+  }
+
+  for (var i = 0; i < lettersGuessed.length; i++) {
+    guessedLetter = lettersGuessed[i];
+    if(guessedLetter === req.body.userGuess){
+      messages.push("Try Again, You've Already Guessed That");
+      match = true;
+    }
+  }
+
+  if (match === false) {
+    guessesLeft = guessesLeft - 1;
+  }
+
+
+
+  if(req.body.userGuess){
+    lettersGuessed.push(req.body.userGuess)
+  }
+
+  if (guessesLeft === 0) {
+    res.redirect("/gameover");
+
+  }
+
+  for (var i = 0; i < underscores.length; i++) {
+    let underscore = underscores[i];
+    if (underscores.indexOf("_") < 0) {
+      res.redirect("/winner");
+    } else {
+    res.redirect("/")
+    }
+  }
+
+});
+
+app.post("/gameover", function(req, res){
+  req.session.destroy();
+  word = words[Math.floor(Math.random()*words.length)];
+  letters = word.split("");
+  underscores = [];
+  letters.forEach(function(newUnderscore){
+    underscores.push("_");
+  });
+  lettersGuessed = [];
+  guessesLeft = 8;
+  res.redirect("/");
+
+});
+
+app.post("/winner", function(req, res){
+  req.session.destroy();
+  word = words[Math.floor(Math.random()*words.length)];
+  letters = word.split("");
+  underscores = [];
+  letters.forEach(function(newUnderscore){
+    underscores.push("_");
+  });
+  lettersGuessed = [];
+  guessesLeft = 8;
+  res.redirect("/");
+});
+
+app.listen(3000, function(){
+  console.log("your app is running on localhost:3000");
+});
---
+++ public/main.js
@@ -0,0 +1,26 @@
+// let wordLetters = document.querySelectorAll(".letter");
+// let userGuess = document.querySelector("#guess");
+// console.log(userGuess);
+// console.log(wordLetters);
+// // wordLetters.style.display = "none";
+// //
+// // function onClick(){
+// //   wordLetters.forEach(function(letterGuess){
+// //     if (letterGuess === userGuess.value) {
+// //       letterGuess.style.display = "block";
+// //     }
+// //     else {
+// //       letterGuess.style.display = "none";
+// //     }
+// // })
+// // }
+//
+// function onClick(){
+//   for (var i = 0; i < wordLetters.length; i++) {
+//     let letter = wordLetters[i];
+//     console.log(wordLetters);
+//     if(userGuess.value === letter.textContent) {
+//       letter.classList.toggle("hidden");
+//     }
+//   }
+// }
